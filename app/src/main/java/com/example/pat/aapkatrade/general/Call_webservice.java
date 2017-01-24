ef62cc3be703163c2ed5dea98fe6396061d0a4ff
PPC21 <nishant@aapkatrade.com>
2017-01-24 15:21:37 +0530

    nishant

---
+++ app/src/main/java/com/example/pat/aapkatrade/general/Call_webservice.java
@@ -0,0 +1,192 @@
+package com.example.pat.aapkatrade.general;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.util.HashMap;
+
+/**
+ * Created by PPC21 on 24-Jan-17.
+ */
+
+public class Call_webservice  {
+
+    static JsonObject jsonObject = new JsonObject();
+   public  static TaskCompleteReminder taskCompleteReminder=null;
+
+    public static void getcountrystatedata(Context c,String webservicetype, String webservice_url, HashMap<String,String> body_parameter,HashMap<String,String> headers) {
+
+        if (webservicetype.equals("country"))
+
+        {
+
+
+            HashMap<String, String> webservice_body_parameter = body_parameter;
+            String authorization = webservice_body_parameter.get("authorization");
+            if (authorization.equals(null)) {
+                Log.e("authorization null", "authorization null");
+            } else {
+
+                String get_webservice_body_parameter = webservice_body_parameter.get("type");
+                String get_webservice_body_parameter_authorization = webservice_body_parameter.get("authorization");
+
+                if (get_webservice_body_parameter.equals(null)) {
+                    Log.e("body_parameter null", "body_parameter null");
+
+                } else {
+
+
+                    HashMap<String, String> webservice_header_type = headers;
+                    webservice_header_type.get("authorization");
+
+
+                    Ion.with(c)
+                            .load(webservice_url)
+                            .setHeader("authorization", authorization)
+                            .setBodyParameter("authorization", get_webservice_body_parameter_authorization)
+                            .setBodyParameter("type", webservicetype)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+
+                                    taskCompleteReminder.Taskcomplete(result);
+                                    Log.e("jsonobject", jsonObject.toString());
+                                    //taskCompleteReminder.Taskcomplete("complete");
+//
+                                }
+
+                            });
+                }
+            }
+
+
+        }
+        else if(webservicetype.equals("state"))
+        {
+            HashMap<String, String> webservice_body_parameter = body_parameter;
+            String authorization = webservice_body_parameter.get("authorization");
+            if (authorization.equals(null)) {
+                Log.e("authorization null", "authorization null");
+            } else {
+
+                String get_webservice_body_parameter = webservice_body_parameter.get("type");
+                String get_webservice_body_parameter_authorization = webservice_body_parameter.get("authorization");
+                String country_id = webservice_body_parameter.get("id");
+
+                if (get_webservice_body_parameter.equals(null)) {
+                    Log.e("body_parameter null", "body_parameter null");
+
+                } else {
+
+
+                    HashMap<String, String> webservice_header_type = headers;
+                    webservice_header_type.get("authorization");
+
+
+                    Ion.with(c)
+                            .load(webservice_url)
+                            .setHeader("authorization", authorization)
+                            .setBodyParameter("authorization", get_webservice_body_parameter_authorization)
+                            .setBodyParameter("type", webservicetype)
+                            .setBodyParameter("id", country_id)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+
+                                    taskCompleteReminder.Taskcomplete(result);
+                                    Log.e("jsonobject_state", jsonObject.toString());
+                                    //taskCompleteReminder.Taskcomplete("complete");
+//
+                                }
+
+                            });
+                }
+            }
+
+        }
+        else if(webservicetype.equals("city"))
+        {
+            HashMap<String, String> webservice_body_parameter = body_parameter;
+            String authorization = webservice_body_parameter.get("authorization");
+            if (authorization.equals(null)) {
+                Log.e("authorization null", "authorization null");
+            } else {
+
+                String get_webservice_body_parameter = webservice_body_parameter.get("type");
+                String get_webservice_body_parameter_authorization = webservice_body_parameter.get("authorization");
+                String state_id = webservice_body_parameter.get("id");
+
+                if (get_webservice_body_parameter.equals(null)) {
+                    Log.e("body_parameter null", "body_parameter null");
+
+                } else {
+
+
+                    HashMap<String, String> webservice_header_type = headers;
+                    webservice_header_type.get("authorization");
+
+
+                    Ion.with(c)
+                            .load(webservice_url)
+                            .setHeader("authorization", authorization)
+                            .setBodyParameter("authorization", get_webservice_body_parameter_authorization)
+                            .setBodyParameter("type", webservicetype)
+                            .setBodyParameter("id", state_id)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+
+                                    taskCompleteReminder.Taskcomplete(result);
+                                    Log.e("jsonobject_city", jsonObject.toString());
+                                    //taskCompleteReminder.Taskcomplete("complete");
+//
+                                }
+
+                            });
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
