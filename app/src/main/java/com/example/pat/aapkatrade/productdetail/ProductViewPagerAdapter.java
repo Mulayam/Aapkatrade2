8a3373bd0dcc9d7105dab57efc3e2df1da1761b1
nishantmalaiya <nishantmalaiya@gmail.com>
2017-03-06 18:32:07 +0530

    hello

--- app/src/main/java/com/example/pat/aapkatrade/productdetail/ProductViewPagerAdapter.java
+++ app/src/main/java/com/example/pat/aapkatrade/productdetail/ProductViewPagerAdapter.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import com.example.pat.aapkatrade.R;
+import com.example.pat.aapkatrade.general.Tabletsize;
 import com.koushikdutta.ion.Ion;
 import java.util.ArrayList;
 
@@ -51,16 +52,49 @@ public class ProductViewPagerAdapter extends PagerAdapter
     }
 
     public Object instantiateItem(ViewGroup container, int position) {
-        View itemView = LayoutInflater.from(mContext).inflate(R.layout.viewpager_item, container, false);
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.viewpager_product_detail, container, false);
 
-        ImageView imageView = (ImageView) itemView.findViewById(R.id.img_pager_item);
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.img_viewpager_detail);
 
         System.out.println("position============" + position);
+        if(Tabletsize.isTablet(mContext))
+        {
+            String product_imageurl=imageurl.get(position).replace("small","large");
 
-        Ion.with(imageView)
-                .error(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
-                .placeholder(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
-                .load(imageurl.get(position));
+            Ion.with(imageView)
+                    .error(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+                    .placeholder(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+                    .load(product_imageurl);
+            Log.e("image_large","image_large");
+
+        }
+        else if(Tabletsize.isMedium(mContext))
+        {
+            String product_imageurl=imageurl.get(position).replace("small","medium");
+
+            Ion.with(imageView)
+                    .error(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+                    .placeholder(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+                    .load(product_imageurl);
+            Log.e("image_medium","image_medium");
+
+        }
+        else if(Tabletsize.isSmall(mContext))
+        {
+
+
+            Ion.with(imageView)
+                    .error(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+                    .placeholder(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+                    .load(imageurl.get(position));
+
+            Log.e("image_small","image_small");
+        }
+
+//        Ion.with(imageView)
+//                .error(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+//                .placeholder(ContextCompat.getDrawable(mContext, R.drawable.ic_applogo1))
+//                .load(imageurl.get(position));
         // imageView.setImageResource(R.drawable.banner_home);
 
         container.addView(itemView);
